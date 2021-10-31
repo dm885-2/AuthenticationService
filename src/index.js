e00b22e8f6963c4ee6810f49b1b15ce3fae13eeb
Michael <clonex_kontakt@hotmail.com>
2021-10-31 20:52:14 +0100

    Added mockup function && messed around with rapid-river lib

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
-import subscribe from "@ovcina/rapidriver";
-// import { SignJWT } from 'jose';
+import river from "@ovcina/rapidriver";
+import jwt from "jsonwebtoken";
 
+const SECRET = process.env.SECRET ?? `3(?<,t2mZxj$5JT47naQFTXwqNWP#W>'*Kr!X!(_M3N.u8v}%N/JYGHC.Zwq.!v-`;  // JWT Token
+const host = process.env.riverUrl ?? `amqp://localhost`;  // RabbitMQ url
 
+/**
+ * 
+ * @param {*} username 
+ * @param {*} password 
+ * @returns 
+ */
+async function login(username, password)
+{
+    if(1)
+    {
+        const token = jwt.sign({ 
+            username,
+            rank: 0,
+         }, SECRET, {
+             expiresIn: 60 * 60 * 2,
+             issuer: "",
+         });
+
+        return {
+            error: false,
+            rank: 0,
+            key: token,
+        };
+    }
+
+    return {
+        error: true,
+    };
+}
+
+river.subscribe(host, "auth", async (msg, channel) => {
+    const response = await login(msg.username, msg.password);
+    channel.publish("auth-response", response);
+});
