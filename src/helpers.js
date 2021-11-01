cc55df31baf4f293172144f3bc84e05b62b83d0c
Michael <clonex_kontakt@hotmail.com>
2021-11-01 21:51:06 +0100

    Added refreshTokens && accessToken mockup

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.SECRET ?? `3(?<,t2mZxj$5JT47naQFTXwqNWP#W>'*Kr!X!(_M3N.u8v}%N/JYGHC.Zwq.!v-`;  // JWT secret
+export const host = process.env.riverUrl ?? `amqp://localhost`;  // RabbitMQ url
+
+/**
+ * Returns the token payload if its valid, otherwise it returns false.
+ * @param String token 
+ * @returns Promise<false|TokenData>
+ */
+export function getTokenData(token, secretOrPublicKey = SECRET)
+{
+    return new Promise(resolve => jwt.verify(token, secretOrPublicKey, (err, data) => resolve(err ? false : data)));
+}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,61 @@
 import rapid from "@ovcina/rapidriver";
-import jwt from "jsonwebtoken";
+import {host, getTokenData} from "./helpers.js";
 
-const SECRET = process.env.SECRET ?? `3(?<,t2mZxj$5JT47naQFTXwqNWP#W>'*Kr!X!(_M3N.u8v}%N/JYGHC.Zwq.!v-`;  // JWT Token
-const host = process.env.riverUrl ?? `amqp://localhost`;  // RabbitMQ url
+const REFRESH_SECRET = process.env.refreshSecret ?? `$[/AJLN;A~djDLh,/kDg?K$Y=*dY44B4)TV*u*X5jjug9#.k>3QLzN;C9K2J36_:`;  // JWT refresh secret
 
-async function login(username, password)
+export async function generateAccessToken(refreshToken)
 {
-    if(1)
-    {
-        const token = jwt.sign({ 
-            username,
-            rank: 0,
-         }, SECRET, {
-             expiresIn: 60 * 60 * 2, // 2 hours
-             issuer: "",
-         });
-
-        return {
-            token,
-        };
-    }
-
-    return {
+    let ret = {
         token: false,
     };
+    const token = await getTokenData(refreshToken, REFRESH_SECRET);
+    if(token)
+    {
+        ret.token = jwt.sign({ 
+            ...token,
+         }, SECRET, {
+             expiresIn: 60 * 15, // 15 minutes
+             issuer: "",
+         });
+    }
+
+    return ret;
 }
 
-rapid.subscribe(host, "auth", async (msg, publish) => {
-    const response = await login(msg.username, msg.password);
-    publish("auth-response", response);
-});
+/**
+ * Returns a RefreshToken if the username and password is valid.
+ * @param {*} username 
+ * @param {*} password 
+ * @returns 
+ */
+export async function login(username, password)
+{
+    let ret = {
+        token: false,
+    };
+    if(1)
+    {
+        ret.token = jwt.sign({ 
+            username,
+            rank: 0,
+         }, REFRESH_SECRET, {
+             issuer: "",
+         });
+    }
+
+    return ret;
+}
+
+
+if(process.env.RAPID === "1")
+{
+    rapid.subscribe(host, "auth", async (msg, publish) => {
+        const response = await login(msg.username, msg.password);
+        publish("auth-response", response);
+    });
+    
+    rapid.subscribe(host, "auth-token", async (msg, publish) => {
+        const response = await generateAccessToken(msg.token);
+        publish("auth-token-response", response);
+    });
+}
