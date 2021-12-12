5a925210d6f417b6891ffe527f62420b4e9d7e9c
Michael <clonex_kontakt@hotmail.com>
2021-12-12 19:41:44 +0100

    Added dummy jest test

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+// import {login} from "./index.js";
+
+test('can pass', () => {
+    // ping({token: "",}, (channel, message) => {
+    //     expect(channel).toBe("pong");
+    //     expect(message.check).toBe(false);
+    // });
+    
+    expect(true).toBe(true);
+});
