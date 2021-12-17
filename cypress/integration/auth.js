097dec8c41ff479bb5c25a7de8da2512df8c7f0b
= <=>
2021-12-18 00:12:33 +0100

    added shoulds

--- cypress/integration/auth.js
+++ cypress/integration/auth.js
@@ -1,24 +1,35 @@
-const time = Date.now();
-const login = {
-    username: `u${time}`,
-    password: `p${time}`,
-};
 
 describe('Authentication', () => {
-    it("Cant login with wrong credentials", () => cy.request('POST', `/auth/login`, login).then((response) => expect(response.body).to.have.property('error', true)));
+
+    beforeEach(()=> {
+        Cypress.env("loginU", "test");
+        Cypress.env("loginP", "test");
+    })
+
+    it("Cant login with wrong credentials", () => {
+        cy.request('POST', "/auth/login", {"username": "qwert", "password": "qwert"})
+        .then((response) => {expect(response.body).to.have.property('error', true)})
+        .its('status').should('eq', 200);
+    });
 
     it("Can create a user", () => {
         cy.request('POST', "/auth/register", {
-            "username": login.username,
-            "password": login.password,
-            "passwordRepeat": login.password
+            "username": Cypress.env("loginU"),
+            "password": Cypress.env("loginP"),
+            "passwordRepeat": Cypress.env("loginP")
         })
-        .then((response) => expect(response.body).to.have.property('error', false))
+        .then((response) => {
+            expect(response.body).to.have.property('error', false)
+            return response;
+        })
         .its('status').should('eq', 200);
     });
 
     it("Can login with new credentials", () => {
-         cy.request('POST', "/auth/login", login).then((response) => {
+         cy.request('POST', "/auth/login", {
+             "username" : Cypress.env("loginU"),
+             "password" : Cypress.env("loginP")
+         }).then((response) => {
             expect(response.body).to.have.property('error', false);
             Cypress.env('rtoken', response.body.refreshToken); 
          }).its('status').should('eq', 200);
@@ -27,13 +38,16 @@ describe('Authentication', () => {
     it("Cant use an invalid refreshToken", () => {
         cy.request('POST', "/auth/accessToken", {
             "refreshToken": "some-invalid-token",
-        }).then((response) => expect(response.body).to.have.property('error', true))
+        }).then((response) => {expect(response.body).to.have.property('error', true)})
+        .its('status').should('eq', 200);
     });
 
     it("Can use a valid refreshToken", () => {
         cy.request('POST', "/auth/accessToken", {
             "refreshToken": Cypress.env('rtoken') 
-        }).then((response) => expect(response.body).to.have.property('error', false))
+        })
+        .as("validRT")
+        .then((response) => {expect(response.body).to.have.property('error', false)})
         .its('status').should('eq', 200);
     });
 });
