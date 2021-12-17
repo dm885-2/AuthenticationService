9e98868b5e49e958b5a298d154630f0fd0f7d618
Ognjen Andric <ognjen.andric@gmail.com>
2021-12-17 22:06:28 +0100

    updated auth.js

--- cypress/integration/auth.js
+++ cypress/integration/auth.js
@@ -1,32 +1,31 @@
-const IP = Cypress.env('DEV') == 1 ? "http://localhost:8080" : "http://localhost:3000";
 const time = Date.now();
 const login = {
     username: `u${time}`,
     password: `p${time}`,
 };
-let refreshToken;
 
 describe('Authentication', () => {
-    it("Cant login with wrong credentials", () => cy.request('POST', `${IP}/auth/login`, login).then((response) => expect(response.body).to.have.property('error', true)));
+    it("Cant login with wrong credentials", () => cy.request('POST', `/auth/login`, login).then((response) => expect(response.body).to.have.property('error', true)));
 
-    it("Can create a user", () => cy.request('POST', `${IP}/auth/register`, {
+    it("Can create a user", () => cy.request('POST', `/auth/register`, {
         "username": login.username,
         "password": login.password,
         "passwordRepeat": login.password
     }).then((response) => expect(response.body).to.have.property('error', false)));
 
-    it("Can login with new credentials", () => cy.request('POST', `${IP}/auth/login`, login).then((response) => {
+    it("Can login with new credentials", () => cy.request('POST', `/auth/login`, login).then((response) => {
         expect(response.body).to.have.property('error', false);
-        refreshToken = response.body.refreshToken;
+        Cypress.env('rtoken', response.body.refreshToken); 
+
     }));
 
-    it("Cant use an invalid refreshToken", () => cy.request('POST', `${IP}/auth/accessToken`, {
+    it("Cant use an invalid refreshToken", () => cy.request('POST', `/auth/accessToken`, {
         refreshToken: "some-invalid-token",
     }).then((response) => expect(response.body).to.have.property('error', true)));
 
     it("Can use a valid refreshToken", () => {
-        cy.request('POST', `${IP}/auth/accessToken`, {
-            refreshToken
+        cy.request('POST', `/auth/accessToken`, {
+            refreshToken: Cypress.env('rtoken') 
         }).then((response) => expect(response.body).to.have.property('error', false))
     });
 });
